25a97f3d47d1882f9c419592ce9874b4dba25aab
turbulent93 <sadcloud77@gmail.com>
2023-04-07 02:48:50 +0500

    feat(BeautyQueen/Dtos/Response): add response service dto

---
+++ BeautyQueenApi/Dtos/Response/ResponseServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BeautyQueenApi.Dtos.Response
+{
+    public class ResponseServiceDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Duration { get; set; }
+    }
+}
