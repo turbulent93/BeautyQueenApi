d80099072eb9aceb88a26ee25e26578349e98bc8
turbulent93 <sadcloud77@gmail.com>
2023-04-07 01:57:50 +0500

    feat(BeautyQueen/Data): add employee and service to db context

--- BeautyQueenApi/Data/ApplicationDbContext.cs
+++ BeautyQueenApi/Data/ApplicationDbContext.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BeautyQueenApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeautyQueenApi.Data
 {
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Employee> Employee { get; set; }
+        public DbSet<Service> Service { get; set; }
     }
 }
