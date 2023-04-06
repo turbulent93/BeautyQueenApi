e52dfde7761363ee5f345a4e2a46e61bcc26c756
turbulent93 <sadcloud77@gmail.com>
2023-04-07 01:53:02 +0500

    feat(BeautyQueenApi/Models): add service model

---
+++ BeautyQueenApi/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeautyQueenApi.Models
+{
+    public class Service
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Duration { get; set; }
+    }
+}
