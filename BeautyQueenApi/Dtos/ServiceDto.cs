bc76f56beb74e8154b51415195a8f7de3810aae3
turbulent93 <sadcloud77@gmail.com>
2023-04-07 02:46:58 +0500

    feat(BeautyQueenApi/Dtos): add service dto

---
+++ BeautyQueenApi/Dtos/ServiceDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BeautyQueenApi.Dtos
+{
+    public class ServiceDto
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int Duration { get; set; }
+    }
+}
