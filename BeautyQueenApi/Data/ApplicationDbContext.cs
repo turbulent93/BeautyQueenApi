3c4e34950e972c9c5574d603ec17a7c3d34091a2
turbulent93 <sadcloud77@gmail.com>
2023-04-07 01:07:31 +0500

    feat(BeautyQueenApi/Data): add db context

---
+++ BeautyQueenApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BeautyQueenApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
