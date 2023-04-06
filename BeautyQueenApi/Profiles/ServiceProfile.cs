b14ec6717463e5279bc78c247afe38e04da0f2d2
turbulent93 <sadcloud77@gmail.com>
2023-04-07 02:52:48 +0500

    feat(BeautyQUeenApi/Profiles): create service profile

---
+++ BeautyQueenApi/Profiles/ServiceProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Response;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class ServiceProfile : Profile
+    {
+        public ServiceProfile()
+        {
+            CreateMap<ServiceDto, ServiceDto>();
+            CreateMap<Service, ResponseServiceDto>();
+        }
+    }
+}
