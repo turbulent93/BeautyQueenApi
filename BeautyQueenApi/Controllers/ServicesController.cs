76bc5fca4f3e2f34460a917c73f41331d40b83af
turbulent93 <sadcloud77@gmail.com>
2023-04-07 02:42:04 +0500

    feat(BeautyQueenApi/Controllers): generate services controller

---
+++ BeautyQueenApi/Controllers/ServicesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BeautyQueenApi.Data;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Controllers
+{
+    [Route("api/services")]
+    [ApiController]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ServicesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Service>>> GetService()
+        {
+          if (_context.Service == null)
+          {
+              return NotFound();
+          }
+            return await _context.Service.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Service>> GetService(int id)
+        {
+          if (_context.Service == null)
+          {
+              return NotFound();
+          }
+            var service = await _context.Service.FindAsync(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return service;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutService(int id, Service service)
+        {
+            if (id != service.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(service).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Service>> PostService(Service service)
+        {
+          if (_context.Service == null)
+          {
+              return Problem("Entity set 'ApplicationDbContext.Service'  is null.");
+          }
+            _context.Service.Add(service);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetService", new { id = service.Id }, service);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            if (_context.Service == null)
+            {
+                return NotFound();
+            }
+            var service = await _context.Service.FindAsync(id);
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            _context.Service.Remove(service);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ServiceExists(int id)
+        {
+            return (_context.Service?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
