e80efcde44981e30319bb1e477ad8423484977a2
turbulent93 <sadcloud77@gmail.com>
2023-04-11 19:54:15 +0500

    feat(BeautyQueenApi.Migrartions): add update employee migration

---
+++ BeautyQueenApi/Migrations/20230411144438_UpdateEmployee.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeautyQueenApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Image",
+                table: "Employee",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Employee");
+        }
+    }
+}
