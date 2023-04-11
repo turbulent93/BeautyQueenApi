8351679fabfbf28468fa014d80ed72a305581c67
turbulent93 <sadcloud77@gmail.com>
2023-04-11 21:19:55 +0500

    feat(BeautyQueenApi.Controllers): add schedule controller

---
+++ BeautyQueenApi/Controllers/SchedulesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BeautyQueenApi.Data;
+using BeautyQueenApi.Models;
+using BeautyQueenApi.Services;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+
+namespace BeautyQueenApi.Controllers
+{
+    [Route("api/sschedules")]
+    [ApiController]
+    public class SchedulesController : ControllerBase
+    {
+        private readonly IScheduleService _scheduleService;
+
+        public SchedulesController(IScheduleService scheduleService)
+        {
+            _scheduleService = scheduleService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Schedule>>> GetSchedule()
+        {
+            try
+            {
+                return Ok(await _scheduleService.Get());
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Schedule>> PostSchedule(RequestScheduleDto scheduleDto)
+        {
+            try
+            {
+                ScheduleDto schedule = await _scheduleService.Post(scheduleDto);
+
+                return CreatedAtAction(nameof(PostSchedule), new { id = schedule.Id }, schedule);
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchedule(int id)
+        {
+            try
+            {
+                await _scheduleService.Delete(id);
+
+                return NoContent();
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
