ed8bc4d3d05a1ef25deaf55635269a8ec3b228d7
turbulent93 <sadcloud77@gmail.com>
2023-04-12 01:12:56 +0500

    feat(BeautyQueenApi.Dtos): add appointment dtos

---
+++ BeautyQueenApi/Dtos/AppointmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BeautyQueenApi.Dtos
+{
+    public class AppointmentDto
+    {
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public int ScheduleId { get; set; }
+        public TimeOnly StartAt { get; set; }
+        public TimeOnly EndAt { get; set; }
+    }
+}
---
+++ BeautyQueenApi/Dtos/Request/RequestAppointmentDto.cs
@@ -0,0 +1,12 @@
+﻿using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Dtos.Request
+{
+    public class RequestAppointmentDto
+    {
+        public int EmployeeId { get; set; }
+        public int ScheduleId { get; set; }
+        public TimeOnly StartAt { get; set; }
+        public TimeOnly EndAt { get; set; }
+    }
+}
