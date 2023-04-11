77f3c8fe42b4dc4e8c24705287320841ce2ac94e
turbulent93 <sadcloud77@gmail.com>
2023-04-12 01:13:23 +0500

    feat(BeautyQueenApi.Profiles): add appointment profile

---
+++ BeautyQueenApi/Profiles/AppointmentProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class AppointmentProfile : Profile
+    {
+        public AppointmentProfile()
+        {
+            CreateMap<RequestAppointmentDto, Appointment>();
+            CreateMap<Appointment, AppointmentDto>();
+        }
+    }
+}
