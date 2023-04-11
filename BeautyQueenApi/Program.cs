9eee9805d04c979c6cb80852acbc796464185223
turbulent93 <sadcloud77@gmail.com>
2023-04-11 21:18:59 +0500

    feat(beautyQueenApi): add date only converter to json options in program

--- BeautyQueenApi/Program.cs
+++ BeautyQueenApi/Program.cs
@@ -18,6 +18,10 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
+builder.Services.AddControllers().AddJsonOptions(x => {
+    x.JsonSerializerOptions.Converters.Add(new DateOnlyJsonConverter());
+});
+
 builder.Services.AddTransient<IServiceService, ServiceService>();
 builder.Services.AddTransient<ISpecializationService, SpecializationService>();
 builder.Services.AddTransient<IEmployeeService, EmployeeService>();
