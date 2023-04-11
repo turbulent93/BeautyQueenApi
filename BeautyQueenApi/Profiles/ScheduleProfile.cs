47e79888ee66a902ab2042dfd775d1535bc6a7f2
turbulent93 <sadcloud77@gmail.com>
2023-04-11 21:15:06 +0500

    feat(BeautyQueenApi.Profiles): add schedule profile

---
+++ BeautyQueenApi/Profiles/ScheduleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class ScheduleProfile : Profile
+    {
+        public ScheduleProfile()
+        {
+            CreateMap<Schedule, ScheduleDto>();
+            CreateMap<RequestScheduleDto, Schedule>();
+        }
+    }
+}
