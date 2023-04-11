b4f494f64e332141722a3a1509c960dea628c37b
turbulent93 <sadcloud77@gmail.com>
2023-04-11 21:16:23 +0500

    feat(BeautyQueenApi.Services): add schedule service

---
+++ BeautyQueenApi/Services/IScheduleService.cs
@@ -0,0 +1,15 @@
+﻿using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Services
+{
+    public interface IScheduleService
+    {
+        Task<IEnumerable<ScheduleDto>> Get();
+        Task<ScheduleDto> Post(RequestScheduleDto scheduleDto);
+        Task Delete(int id);
+        Task<Schedule?> Find(int id);
+        bool ServiceExists(int id);
+    }
+}
---
+++ BeautyQueenApi/Services/ScheduleService.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Data;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeautyQueenApi.Services
+{
+    public class ScheduleService : IScheduleService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+        public ScheduleService(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ScheduleDto>> Get()
+        {
+            if(_context.Schedule == null)
+            {
+                throw new Exception("Context Scheudle is null");
+            }
+
+            IEnumerable<Schedule> schedules = await _context.Schedule.ToListAsync();
+
+            return _mapper.Map<IEnumerable<ScheduleDto>>(schedules);
+        }
+        public async Task<ScheduleDto> Post(RequestScheduleDto scheduleDto)
+        {
+            if (_context.Schedule == null)
+            {
+                throw new Exception("Context Scheudle is null");
+            }
+
+            Schedule schedule = _mapper.Map<Schedule>(scheduleDto);
+
+            _context.Schedule.Add(schedule);
+
+            await _context.SaveChangesAsync();
+
+            if (!ServiceExists(schedule.Id))
+            {
+                throw new Exception("Service does not add");
+            }
+
+            return _mapper.Map<ScheduleDto>(schedule);
+        }
+
+        public async Task Delete(int id)
+        {
+            Schedule? schedule = await Find(id);
+
+            if (schedule == null)
+            {
+                throw new Exception("Schedule is not found");
+            }
+
+            _context.Schedule.Remove(schedule);
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Schedule?> Find(int id)
+        {
+            return await _context.Schedule.FindAsync(id);
+        }
+
+        public bool ServiceExists(int id)
+        {
+            return (_context.Schedule?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
