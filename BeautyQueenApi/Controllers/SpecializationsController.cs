2b27120f024f67a30a2dfcca36ea6d6bdf68f7eb
turbulent93 <sadcloud77@gmail.com>
2023-04-11 19:16:14 +0500

    feat(BeautyQueenApi.Controllers): add specialization controller

---
+++ BeautyQueenApi/Controllers/SpecializationsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BeautyQueenApi.Data;
+using BeautyQueenApi.Models;
+using BeautyQueenApi.Services;
+using BeautyQueenApi.Dtos.Request;
+
+namespace BeautyQueenApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecializationsController : ControllerBase
+    {
+        private readonly ISpecializationService _specializationService;
+
+        public SpecializationsController(ISpecializationService specializationService)
+        {
+            _specializationService = specializationService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Specialization>>> GetSpecialization()
+        {
+            try
+            {
+                return Ok(await _specializationService.Get());
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSpecialization(int id, Specialization specialization)
+        {
+            try
+            {
+                await _specializationService.Put(id, specialization);
+                return NoContent();
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Specialization>> PostSpecialization(RequestSpecializationDto specializationDto)
+        {
+            try
+            {
+                Specialization specialization = await _specializationService.Post(specializationDto);
+                return CreatedAtAction("GetSpecialization", new { id = specialization.Id }, specialization);
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSpecialization(int id)
+        {
+            try
+            {
+                await _specializationService.Delete(id);
+                return NoContent();
+            } catch(Exception)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
