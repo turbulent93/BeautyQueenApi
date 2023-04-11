a45cf35d8e7017502e68d78e9394a457dcbf6979
turbulent93 <sadcloud77@gmail.com>
2023-04-11 19:13:54 +0500

    feat(BeautyQuaanApi.Profiles): add specialization profile

---
+++ BeautyQueenApi/Profiles/SpecializationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class SpecializationProfile : Profile
+    {
+        public SpecializationProfile()
+        {
+            CreateMap<Specialization, RequestSpecializationDto>()
+                .ReverseMap();
+        }
+    }
+}
