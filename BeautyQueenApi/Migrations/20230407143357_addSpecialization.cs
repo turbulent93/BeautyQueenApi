d0f7daa8d6e4e83bbf56b89d998ec87a79cb69bc
turbulent93 <sadcloud77@gmail.com>
2023-04-11 19:03:34 +0500

    feat(BeautyQueenApi.Migrations): add specialization migration

---
+++ BeautyQueenApi/Migrations/20230407143357_addSpecialization.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BeautyQueenApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSpecialization : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SpecializationId",
+                table: "Employee",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Specialization",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialization", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_SpecializationId",
+                table: "Employee",
+                column: "SpecializationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employee_Specialization_SpecializationId",
+                table: "Employee",
+                column: "SpecializationId",
+                principalTable: "Specialization",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employee_Specialization_SpecializationId",
+                table: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Specialization");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employee_SpecializationId",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "SpecializationId",
+                table: "Employee");
+        }
+    }
+}
