5e73c26a65ccc67437014283a5bcad42c8393e1c
turbulent93 <sadcloud77@gmail.com>
2023-04-12 01:11:49 +0500

    feat(BeautyQueenApi.Models): add appointment model

---
+++ BeautyQueenApi/Models/Appointment.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BeautyQueenApi.Models
+{
+    public class Appointment
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+        public int ScheduleId { get; set; }
+        public Schedule Schedule { get; set; }
+        public TimeOnly StartAt { get; set; }
+        public TimeOnly EndAt { get; set; }
+    }
+}
