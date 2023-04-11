115dde059ff3ba0947754cbd1ae821bd2c316b65
turbulent93 <sadcloud77@gmail.com>
2023-04-11 19:53:07 +0500

    feat(BeautyQueenApi.Models): add image and specialization to employee

--- BeautyQueenApi/Models/Employee.cs
+++ BeautyQueenApi/Models/Employee.cs
@@ -8,6 +8,9 @@ namespace BeautyQueenApi.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public int SpecializationId { get; set; }
+        public Specialization Specialization { get; set; }
+        public string Image { get; set; }
         public List<Service> Services { get; set; }
     }
 }
