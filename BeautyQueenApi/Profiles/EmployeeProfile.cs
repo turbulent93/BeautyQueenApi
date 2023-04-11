3c354de4b5e0bf3acef7b6ca07dfcf4290441bf7
turbulent93 <sadcloud77@gmail.com>
2023-04-11 20:01:18 +0500

    feat(BeautyQueenApi.Profiles): add employee profile

---
+++ BeautyQueenApi/Profiles/EmployeeProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<RequestEmployeeDto, Employee>()
+                .ForMember(dist => dist.Image, opt => opt.MapFrom(e => e.Image.FileName));
+            CreateMap<RequestPutEmployeeDto, Employee>()
+                .ForMember(dist => dist.Image, opt => opt.MapFrom(e => e.Image.FileName));
+            CreateMap<Employee, EmployeeDto>();
+        }
+    }
+}
