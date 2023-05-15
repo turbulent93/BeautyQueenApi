d2e50736c26bbec51be657f90eaed5027201bf6c
turbulent93 <sadcloud77@gmail.com>
2023-05-15 23:03:11 +0500

    feat(BeautyQueenApi.Controllers): add admin controller

---
+++ BeautyQueenApi/Controllers/AdminController.cs
@@ -0,0 +1,71 @@
+﻿using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Models;
+using BeautyQueenApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace BeautyQueenApi.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+        [HttpGet("users")]
+        public async Task<ActionResult<IEnumerable<User>>> Get(string? registered)
+        {
+            try
+            {
+                IEnumerable<User> users = await _adminService.Get(registered);
+                return Ok(users.ToList());
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        //[Authorize]
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterDto adminDto) {
+            try
+            {
+                await _adminService.Register(adminDto);
+                return Ok("Success");
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthDto>> Login(RegisterDto adminDto)
+        {
+            try
+            {
+                return Ok(await _adminService.Login(adminDto));
+            } catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<AuthDto>> RefreshToken(AuthDto loginDto)
+        {
+            try
+            {
+                return Ok(await _adminService.RefreshToken(loginDto));
+            } catch(Exception e)
+            {
+                return Unauthorized(e.Message);
+            }
+        }
+    }
+}
