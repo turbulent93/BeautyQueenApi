2d6d2f1f8784d81405ae9add02c2e5fc08e81f0b
turbulent93 <sadcloud77@gmail.com>
2023-05-15 22:55:41 +0500

    feat(BeautyQueenApi.Profiles): add admin profile

---
+++ BeautyQueenApi/Profiles/AdminProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BeautyQueenApi.Dtos;
+using BeautyQueenApi.Dtos.Request;
+using BeautyQueenApi.Models;
+
+namespace BeautyQueenApi.Profiles
+{
+    public class AdminProfile : Profile
+    {
+        public AdminProfile()
+        {
+            CreateMap<RequestEmployeeDto, User>();
+            CreateMap<RegisterDto, User>()
+                .ForMember(dist => dist.Password, opt => opt.MapFrom(e => BCrypt.Net.BCrypt.HashPassword(e.Password)));
+        }
+    }
+}
