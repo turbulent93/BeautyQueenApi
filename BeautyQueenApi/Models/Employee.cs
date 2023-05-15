87ad6cf1742e181bcf152a1d4cf8b0cd51481591
turbulent93 <sadcloud77@gmail.com>
2023-05-15 22:48:02 +0500

    feat(BeautyQueenApi.Models): add auth models and update employee

--- BeautyQueenApi/Models/Employee.cs
+++ BeautyQueenApi/Models/Employee.cs
@@ -1,16 +1,23 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace BeautyQueenApi.Models
 {
     public class Employee
     {
         [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        [JsonIgnore]
         public int SpecializationId { get; set; }
         public Specialization Specialization { get; set; }
         public string Image { get; set; }
         public List<Service> Services { get; set; }
+        public int UserId { get; set; }
+        [JsonIgnore]
+        public User User { get; set; }
     }
 }
---
+++ BeautyQueenApi/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BeautyQueenApi.Models
+{
+    [Index(nameof(Login), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? ExpiresIn { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
