7b3178c1d4a1eacf8f457a050d19845fd3af7ee2
turbulent93 <sadcloud77@gmail.com>
2023-05-24 16:46:49 +0500

    refact(BeautyQueenApi.Profiles): remove formember from image map

--- BeautyQueenApi/Profiles/EmployeeProfile.cs
+++ BeautyQueenApi/Profiles/EmployeeProfile.cs
@@ -9,10 +9,8 @@ namespace BeautyQueenApi.Profiles
     {
         public EmployeeProfile()
         {
-            CreateMap<RequestEmployeeDto, Employee>()
-                .ForMember(dist => dist.Image, opt => opt.MapFrom(e => e.Image.FileName));
-            CreateMap<RequestPutEmployeeDto, Employee>()
-                .ForMember(dist => dist.Image, opt => opt.MapFrom(e => e.Image.FileName));
+            CreateMap<RequestEmployeeDto, Employee>();
+            CreateMap<RequestPutEmployeeDto, Employee>();
             CreateMap<Employee, EmployeeDto>();
         }
     }
