7dc00a44bfa208910506c29cab5e6b227c638711
turbulent93 <sadcloud77@gmail.com>
2023-05-24 16:41:05 +0500

    feat(BeautyQueenApi.Dtos): add specialization and list service to employee dto

--- BeautyQueenApi/Dtos/EmployeeDto.cs
+++ BeautyQueenApi/Dtos/EmployeeDto.cs
@@ -7,7 +7,8 @@ namespace BeautyQueenApi.Dtos
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public int SpecializationId { get; set; }
+        public Specialization Specialization { get; set; }
+        public List<Service> Services { get; set; }
         public string Image { get; set; }
     }
 }
