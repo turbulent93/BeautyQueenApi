a6da3485c800503fa6729072c6cfbeff1bcf23df
turbulent93 <sadcloud77@gmail.com>
2023-05-24 16:39:25 +0500

    refact(BeautyQueenApi.Dtos.Request): replace image type to string and add user id

--- BeautyQueenApi/Dtos/Request/RequestEmployeeDto.cs
+++ BeautyQueenApi/Dtos/Request/RequestEmployeeDto.cs
@@ -5,7 +5,8 @@
         public string Name { get; set; }
         public string Surname { get; set; }
         public int SpecializationId { get; set; }
-        public IFormFile Image { get; set; }
+        public string Image { get; set; }
         public List<int> ServiceIds { get; set; }
+        public int UserId { get; set; }
     }
 }
