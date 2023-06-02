c1d7a798d7fd35ed2d97458f06ad7359302720e1
turbulent93 <sadcloud77@gmail.com>
2023-06-02 09:43:28 +0500

    feat(BeautyQueenApi.Dtos.Request): add service id and photo to dto

--- BeautyQueenApi/Dtos/Request/RequestAppointmentDto.cs
+++ BeautyQueenApi/Dtos/Request/RequestAppointmentDto.cs
@@ -6,7 +6,9 @@ namespace BeautyQueenApi.Dtos.Request
     {
         public int EmployeeId { get; set; }
         public int ScheduleId { get; set; }
+        public int ServiceId { get; set; }
         public TimeOnly StartAt { get; set; }
         public TimeOnly EndAt { get; set; }
+        public string Phone { get; set; }
     }
 }
