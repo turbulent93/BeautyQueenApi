75eed9db0ad5f68a956eb1c6605ea7c682890cfc
turbulent93 <sadcloud77@gmail.com>
2023-06-02 10:03:38 +0500

    feat(BeautyQueenApi.Interfaces) add search to get and add get by service

--- BeautyQueenApi/Interfaces/IEmployeeService.cs
+++ BeautyQueenApi/Interfaces/IEmployeeService.cs
@@ -6,14 +6,14 @@ namespace BeautyQueenApi.Interfaces
 {
     public interface IEmployeeService
     {
-        Task<IEnumerable<EmployeeDto>> Get();
+        Task<IEnumerable<EmployeeDto>> Get(string? search);
         Task<EmployeeDto> Post(RequestEmployeeDto employeeDto);
         Task Put(int id, RequestPutEmployeeDto employeeDto);
         Task Delete(int id);
         bool EmployeeExists(int id);
         Task<Employee?> Find(int id);
         Task<string> AddImage(IFormFile image);
-        Task<Employee> GetById(int id);
+        Task<IEnumerable<EmployeeDto>> GetByService(int id, string? search);
         Task SetServicesByIds(Employee employee, List<int> serviceIds);
     }
 }
