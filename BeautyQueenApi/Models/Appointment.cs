d0b3933ca6ea7032dc303d2131e8425a682b0002
turbulent93 <sadcloud77@gmail.com>
2023-06-02 09:40:54 +0500

    feat(BeautyQueenApi.Models): add service and phone to appointment

--- BeautyQueenApi/Models/Appointment.cs
+++ BeautyQueenApi/Models/Appointment.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace BeautyQueenApi.Models
 {
@@ -6,11 +7,17 @@ namespace BeautyQueenApi.Models
     {
         [Key]
         public int Id { get; set; }
+        //[JsonIgnore]
         public int EmployeeId { get; set; }
         public Employee Employee { get; set; }
+        //[JsonIgnore]
         public int ScheduleId { get; set; }
         public Schedule Schedule { get; set; }
+        //[JsonIgnore]
+        public int ServiceId { get; set; }
+        public Service Service { get; set; }
         public TimeOnly StartAt { get; set; }
         public TimeOnly EndAt { get; set; }
+        public string Phone { get; set; }
     }
 }
